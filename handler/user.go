7cae3ae93a53cae4694a1b223ed2164c53dedf0a
Victor The <victorthe123@gmail.com>
2023-08-18 21:52:22 +0700

    [Backend] Register User Endpoint - Error Formatter

--- handler/user.go
+++ handler/user.go
@@ -21,12 +21,16 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.ApiResponse("Account register failed!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.ApiResponse("Account register failed!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	formatter := user.FormatUser(newUser, "tokentokentokentoken")
